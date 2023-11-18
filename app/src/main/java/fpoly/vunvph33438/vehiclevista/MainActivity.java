830219887d23d3e7a622afc7043c9029a3426a80
Nguyễn Văn Vũ <vanvu101204@gmail.com>
2023-11-18 15:06:37 +0700

    Them layout login, splash, signup, main

--- app/src/main/java/fpoly/vunvph33438/vehiclevista/MainActivity.java
+++ app/src/main/java/fpoly/vunvph33438/vehiclevista/MainActivity.java
@@ -1,14 +1,52 @@
 package fpoly.vunvph33438.vehiclevista;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String username = "admin"; // replace with actual username
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nvBottom);
+
+        if ("admin".equals(username)) {
+            // Admin user, inflate admin menu
+            bottomNavigationView.inflateMenu(R.menu.drawer_view_admin);
+        } else {
+            // Regular user, inflate regular menu
+            bottomNavigationView.inflateMenu(R.menu.drawer_view_user);
+        }
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            // Handle navigation item clicks here
+            int itemId = item.getItemId();
+            if (itemId == R.id.nav_loaiXe) {
+                // Handle the click for nav_loaiXe
+            } else if (itemId == R.id.nav_xe) {
+                // Handle the click for nav_xe
+            } else if (itemId == R.id.nav_lichSu) {
+                // Handle the click for nav_lichSu
+            } else if (itemId == R.id.sub_quanLy) {
+                // Handle the click for sub_quanLy
+            } else if (itemId == R.id.sub_trangChu) {
+                // Handle the click for sub_trangChu
+            } else if (itemId == R.id.nav_lichSuDatXe) {
+                // Handle the click for nav_lichSuDatXe
+            } else if (itemId == R.id.sub_taiKhoan) {
+                // Handle the click for sub_taiKhoan
+            }
+            return true;
+        });
     }
 }
